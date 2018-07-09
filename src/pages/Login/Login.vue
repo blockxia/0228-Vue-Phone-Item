a666d75e50d0919a269c62dc8dc4c2c0b1a95be7
xiaoxia <920634755@qq.com>
2018-07-09 20:36:21 +0800

    login路由，发送手机验证码及按钮切换密码和文字显示更新

--- src/pages/Login/Login.vue
+++ src/pages/Login/Login.vue
@@ -4,16 +4,18 @@
       <div class="login_header">
         <h2 class="login_logo">硅谷外卖</h2>
         <div class="login_header_title">
-          <a href="javascript:;" class="on">短信登录</a>
-          <a href="javascript:;" >密码登录</a>
+          <a href="javascript:;" :class="{on:loginWay}" @click="loginWay=true">短信登录</a>
+          <a href="javascript:;" :class="{on:!loginWay}" @click="loginWay=false">密码登录</a>
         </div>
       </div>
       <div class="login_content">
         <form>
-          <div class="on">
+          <div :class="{on:loginWay}">
             <section class="login_message">
-              <input type="tel" maxlength="11" placeholder="手机号">
-              <button disabled="disabled" class="get_verification" >获取验证码</button>
+              <input type="tel" maxlength="11" placeholder="手机号" v-model="phone">
+              <button  class="get_verification" :class="{right_phone_number:isRightPhone}" @click="sendCode">
+                {{computeTime>0 ? `已发送(${computeTime}s)`: '获取验证码' }}
+                </button>
             </section>
             <section class="login_verification">
               <input type="tel" maxlength="8" placeholder="验证码">
@@ -23,16 +25,16 @@
               <a href="javascript:;">《用户服务协议》</a>
             </section>
           </div>
-          <div>
+          <div :class="{on:!loginWay}">
             <section>
               <section class="login_message">
                 <input type="tel" maxlength="11" placeholder="手机/邮箱/用户名">
               </section>
               <section class="login_verification">
-                <input type="tel" maxlength="8" placeholder="密码">
-                <div class="switch_button off">
-                  <div class="switch_circle"></div>
-                  <span class="switch_text">...</span>
+                <input :type="isHide? 'password' : 'text'" maxlength="8" placeholder="密码">
+                <div class="switch_button" :class="isHide? 'off' :'on'"  @click="isHide=!isHide">
+                  <div class="switch_circle" :class="{right:!isHide}"></div>
+                  <span class="switch_text">{{isHide? '':'abc'}}</span>
                 </div>
               </section>
               <section class="login_message">
@@ -53,7 +55,37 @@
 </template>
 
 <script>
-  export default {}
+  export default {
+    data(){
+      return{
+        loginWay:true,//true:短信
+        phone:'',
+        computeTime:0,
+        isHide:true
+      }
+    },
+    computed:{
+      isRightPhone(){
+        return /^1\d{10}$/.test(this.phone)
+      }
+    },
+    methods:{
+      sendCode(){
+        // 如果是正确的手机号并且没有计时, 才开始倒计时
+        if(this.isRightPhone && this.computeTime===0){
+          this.computeTime=10
+          // 启动循环定时器
+          const timer=setInterval(()=>{
+            this.computeTime--
+            if(this.computeTime===0){
+              // 清除定时器
+              clearInterval(timer)
+            }
+          },1000)
+        }
+      }
+    }
+  }
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
@@ -108,6 +140,7 @@
               font-size 14px
               background #fff
               .get_verification
+                outline none
                 position absolute
                 top 50%
                 right 10px
@@ -116,6 +149,8 @@
                 color #ccc
                 font-size 14px
                 background transparent
+                &.right_phone_number
+                  color black
             .login_verification
               position relative
               margin-top 16px
@@ -155,6 +190,8 @@
                   background #fff
                   box-shadow 0 2px 4px 0 rgba(0,0,0,.1)
                   transition transform .3s
+                  &.right
+                    transform translateX(27px)
             .login_hint
               margin-top 12px
               color #999
